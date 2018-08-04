bf6755c1f636c035a1932f08326b8cc0a54d3845
Yuriy Petrushkin <ynn@athn.ru>
2018-08-04 22:37:49 +0300

    fix: fixed paths for resourses

--- sw.js
+++ sw.js
@@ -2,24 +2,24 @@
 const CACHE = 'restaurant-reviews';
 /** Resources to precache */
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/restaurant.html',
-    '/css/styles.css',
-    '/data/restaurants.json',
-    '/img/1.jpg',
-    '/img/2.jpg',
-    '/img/3.jpg',
-    '/img/4.jpg',
-    '/img/5.jpg',
-    '/img/6.jpg',
-    '/img/7.jpg',
-    '/img/8.jpg',
-    '/img/9.jpg',
-    '/img/10.jpg',
-    '/js/main.js',
-    '/js/restaurant_info.js',
-    '/js/dbhelper.js'
+    './',
+    './index.html',
+    './restaurant.html',
+    './css/styles.css',
+    './data/restaurants.json',
+    './img/1-800_2x.jpg',
+    './img/2-800_2x.jpg',
+    './img/3-800_2x.jpg',
+    './img/4-800_2x.jpg',
+    './img/5-800_2x.jpg',
+    './img/6-800_2x.jpg',
+    './img/7-800_2x.jpg',
+    './img/8-800_2x.jpg',
+    './img/9-800_2x.jpg',
+    './img/10-800_2x.jpg',
+    './js/main.js',
+    './js/restaurant_info.js',
+    './js/dbhelper.js'
 ];
 
 /**
